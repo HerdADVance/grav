3131893761dd627f5a2e925e4104f79404a1ae49
Alex <herdadvance@gmail.com>
2019-10-14 23:25:02 -0400

    Init

---
+++ grav-admin/cache/compiled/files/15a58b102bd36d1d41865549e57653e9.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/m/code/grav/grav-admin/user/config/security.yaml',
+    'modified' => 1571107910,
+    'data' => [
+        'salt' => 'NBjm05lrAfquZw'
+    ]
+];
